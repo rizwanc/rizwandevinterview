1cd1f29048a4d303f399a974bf7c62001e314be5
rizwanc <chaudryrizwan@gmail.com>
2019-07-22 11:52:55 -0400

    Ran migrations to create database tables

--- rizwandevinterviewproject/articles/models.py
+++ rizwandevinterviewproject/articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from authors.models import Author
 
 class Article(models.Model):
 	author_name = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
@@ -14,8 +15,3 @@ class Article(models.Model):
 	photo_4 = models.ImageField(upload_to='photos', blank=True)
 	def __str__(self):
 		return self.title
-
-class Author(models.Model):
-	name = models.CharField(max_length=200)
-	def __str__(self):
-		return self.name
--- rizwandevinterviewproject/rizwandevinterviewproject/settings.py
+++ rizwandevinterviewproject/rizwandevinterviewproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'articles.apps.ArticlesConfig',
+    'authors.apps.AuthorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
