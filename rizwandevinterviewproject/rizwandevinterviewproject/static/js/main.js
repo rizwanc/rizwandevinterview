156fa01dd57f14f6a0b0d4646c3b2b9ce77c951e
rizwanc <chaudryrizwan@gmail.com>
2019-07-24 14:27:55 -0400

    Added comments to javascript

--- rizwandevinterviewproject/rizwandevinterviewproject/static/js/main.js
+++ rizwandevinterviewproject/rizwandevinterviewproject/static/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+	// Variable to get the shuffle button
     var shuffle = document.getElementById('shuffle');
-   	var stocks = $(".ticker-row div");
-   	var randPos1, randPos2;
-   	var addComment = document.getElementById('addCommentButton');
 
+    // Variable to get the div contained in the ticker row
+   	var stocks = $(".ticker-row div");
+
+   	// Used to store the randomly genrated index's
+   	var randPos1, randPos2;
+
+   	// This function loops through the number of items in stocks
+   	// and generates a random index between 1 and 3 and then using
+   	// the eq method the items are swapped.
 	shuffle.onclick = function() {
 		for(var i = 0, length = stocks.length; i < length; i++) {
 			randPos1 = Math.floor(Math.random() * length-1) + 1;
